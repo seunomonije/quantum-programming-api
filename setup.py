3069caad444c276cd7b88483eccbcd60b64c7f51
seunomonije <seunomonije@icloud.com>
2021-03-04 20:26:01 -0500

    starting package setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+  name = 'qapi',
+  version = '0.0.1',
+  description='Harness the power of quantum computing without quantum knowledge',
+  py_modules=[],
+  package_dir={'': 'src'}
+)
