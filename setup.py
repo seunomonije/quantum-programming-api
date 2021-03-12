9467cf89e138eab0ae08e7bb1a378338f7703a0a
seunomonije <seunomonije@icloud.com>
2021-03-12 13:57:47 -0500

    update setup

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import setuptools
 
 setuptools.setup(
   name='qapi',
-  version='0.0.1',
-  author="Seun Omonije, Aidan Evans",
+  version='0.0.2',
+  author="Seun Omonije",
   author_email="seun.omonije@yale.edu",
   description='Harness the power of quantum computing without quantum knowledge.',
   url="https://github.com/seunomonije/quantum-programming-api",
   classifiers=[
     "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
   ],
   license="Apache 2",
   packages=setuptools.find_packages(),
