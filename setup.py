5a64a5dad52efb79561f516a46fbc29bd10e14b5
seunomonije <seunomonije@icloud.com>
2021-03-11 22:59:09 -0500

    refactoring for package distro

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
-from setuptools import setup
-setup(
-  name = 'qapi',
-  version = '0.0.1',
-  description='Harness the power of quantum computing without quantum knowledge',
-  py_modules=[],
-  package_dir={'': 'src'}
+import setuptools
+
+setuptools.setup(
+  name='qapi',
+  version='0.0.1',
+  author="Seun Omonije, Aidan Evans",
+  author_email="seun.omonije@yale.edu",
+  description='Harness the power of quantum computing without quantum knowledge.',
+  url="https://github.com/seunomonije/quantum-programming-api",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: MIT License",
+  ],
+  packages=setuptools.find_packages(),
+  py_modules=["qapi"],
+  package_dir={'':'qapi/src'},
+  python_requires=">3.6",
+  install_requires=[]
 )
