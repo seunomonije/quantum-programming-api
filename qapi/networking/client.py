9e231d083857fa93cff79ddbd3e230a1083e1384
seunomonije <seunomonije@icloud.com>
2021-03-15 15:40:56 -0400

    client example for sending data to and from the engine

---
+++ qapi/networking/client.py
@@ -0,0 +1,30 @@
+import socket
+import select
+import errno
+import pickle
+import sys
+
+HEADERSIZE = 10
+IP_ADDRESS = "localhost"
+PORT = 1212
+
+class InputData:
+    def __init__(self, xVal, yVal):
+        self.xVal = xVal
+        self.yVal = yVal
+        
+        
+xVal = float(input("input x:"))
+yVal = float(input("input y:"))
+
+data = InputData(xVal, yVal)
+
+# Connection starting
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((IP_ADDRESS, PORT))
+
+data_to_send = pickle.dumps(data)
+"""Need to revisit this line later, headers are good"""
+#data_to_send = bytes(f'{len(data_to_send):<{HEADERSIZE}}', "UTF-8") + data_to_send
+#data_to_send = bytes(f'{data_to_send}', 'UTF-8')
+client.send(data_to_send)
